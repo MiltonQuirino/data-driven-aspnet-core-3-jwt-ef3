4e4178ba86dcb7ca9f0e765d1f6c915547942b3d
Milton Quirino <miltonquirino@Miltons-MacBook-Pro.local>
2020-10-18 11:11:26 -0300

    first commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,137 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Backoffice.Models;
+using System;
+using Microsoft.AspNetCore.Authorization;
+using Backoffice.Data;
+using Microsoft.EntityFrameworkCore;
+
+[Route("v1/categories")]
+public class CategoryController : ControllerBase
+{
+
+    [HttpGet]
+    [Route("")]
+    [AllowAnonymous]
+    public async Task<ActionResult<List<Category>>> Get([FromServices] DataContext context)
+    {
+        var categories = await context.Categories.AsNoTracking().ToListAsync();
+        return Ok(categories);
+    }
+
+    [HttpGet]
+    [Route("{id:int}")]
+    [AllowAnonymous]
+    public async Task<ActionResult<Category>> GetById(
+        int id,
+        [FromServices] DataContext context
+    )
+    {
+        try
+        {
+            var category = await context.Categories.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(category);
+
+        }
+        catch (Exception ex)
+        {
+
+            return BadRequest(new { message = $"Categoria não encontrada. Trace : {ex.Message}" });
+        }
+
+    }
+
+    [HttpPost]
+    [Route("")]
+    [Authorize(Roles = "employee")]
+    public async Task<ActionResult<Category>> Post(
+        [FromBody] Category model,
+        [FromServices] DataContext context
+    )
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        try
+        {
+            context.Categories.Add(model);
+            await context.SaveChangesAsync();
+            return Ok(model);
+
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = $"Não foi possível criar a categoria. trace: {ex.Message}" });
+        }
+
+    }
+
+    [HttpPut]
+    [Route("{id:int}")]
+    [Authorize(Roles = "employee")]
+    public async Task<ActionResult<Category>> Put(
+        int id,
+        [FromBody] Category model,
+        [FromServices] DataContext context
+    )
+    {
+
+        if (id != model.Id)
+        {
+            return NotFound(new { message = "Categoria não encontrada" });
+        }
+
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        try
+        {
+            context.Entry<Category>(model).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return model;
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            return BadRequest(new { message = "Não foi possive atualiza a categoria no banco de dados" });
+        }
+        catch (Exception ex)
+        {
+
+            return BadRequest(new { message = $"Não foi possível atualizar a categoria. trace: {ex.Message}" });
+        }
+
+    }
+
+    [HttpDelete]
+    [Route("{id:int}")]
+    [Authorize(Roles = "employee")]
+    public async Task<ActionResult<Category>> Delete(
+        int id,
+        [FromServices] DataContext context
+        )
+    {
+        var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (category == null)
+        {
+            return NotFound(new { message = "Categoria não encontrada" });
+        }
+
+        try
+        {
+            context.Categories.Remove(category);
+            await context.SaveChangesAsync();
+            return Ok(new { message = "Categoria removida com sucesso" });
+        }
+        catch (Exception)
+        {
+            return BadRequest(new { message = "Não foi possivel remover a categoria" });
+        }
+    }
+
+}
---
+++ Startup.cs
@@ -0,0 +1,82 @@
+using System.Linq;
+using System.Text;
+using Backoffice;
+using Backoffice.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Shop
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddResponseCompression(options =>
+            {
+                options.Providers.Add<GzipCompressionProvider>();
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/json" });
+            });
+
+            services.AddControllers();
+
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            // services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+            services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
+            services.AddScoped<DataContext, DataContext>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
